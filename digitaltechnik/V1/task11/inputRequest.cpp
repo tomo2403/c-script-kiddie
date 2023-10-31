d98cc49b20f70294bece92e12026c33fa6a632f8
SponsoredByHartz4 <mo_apps@tuta.io>
2023-10-31 21:18:52 +0100

    improved input validation

--- digitaltechnik/V1/task11/inputRequest.cpp
+++ digitaltechnik/V1/task11/inputRequest.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 int inputRequest()
 {
-    int decimalInput = 0;
     cout << "Max. Byte: ";
-    cin >> decimalInput;
+
+    int decimalInput = 0;
+    if(!(cin >> decimalInput)) //checks if input is numeric
+    {
+        cout << "Die Eingabe muss eine Ganzzahl sein, welche zur Reihe der 2er-Potenzen gehört und nicht größer als 256 ist." << endl;
+        cin.clear();
+        cin.ignore(123, '\n');
+        return inputRequest();
+    }
 
     int i = 1;
-    while (decimalInput <= 256 and decimalInput > i)
+    while (decimalInput <= 256 and decimalInput > i) //finds valid number, closest to the number inputted
     {
         i *= 2;
     }
 
-    if (decimalInput == i)
+    if (decimalInput == i) //checks if inputted number is valid
     {
         return decimalInput;
     }
     else
     {
-        cout << "Eingabe ungültig. Bitte erneut versuchen." << endl;
+        cout << "Die Eingabe muss eine Ganzzahl sein, welche zur Reihe der 2er-Potenzen gehört und nicht größer als 256 ist." << endl;
         return inputRequest();
     }
 }
