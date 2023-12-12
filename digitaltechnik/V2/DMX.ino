e4ca6038213c5477b72cbd304b4cacb269a22850
tomo <49612544+tomo2403@users.noreply.github.com>
2023-12-12 12:37:32 +0100

    added V2 sample

---
+++ digitaltechnik/V2/DMX.ino
@@ -0,0 +1,16 @@
+
+#include "DmxSimple.h"
+
+enum action {huhn, world};
+
+void setup() {
+  DmxSimple.usePin(3);
+  DmxSimple.maxChannel(44);
+}
+
+void loop() {
+  
+  DmxSimple.write(1,255);
+  DmxSimple.write(2,255);  
+ 
+}
