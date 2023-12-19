e25add471830181c19144b90a6817edbb754f1ca
tomo <49612544+tomo2403@users.noreply.github.com>
2023-12-19 10:19:07 +0100

    add project structure for V2

---
+++ digitaltechnik/V2/src/main.cpp
@@ -0,0 +1,77 @@
+#define DEMO 1         //Demomodus AUS bei 0
+#define BPM_IN_MS 484  //60.000ms geteilt durch BPM
+
+#include <Arduino.h>
+#include "../RgbwSpotlight8Ch.h"
+#include "../MiniMovingHead14Ch.h"
+
+#if DEMO == 0
+RgbwSpotlight8Ch spotlight1(1);
+RgbwSpotlight8Ch spotlight2(37);
+MiniMovingHead14Ch mover1(9);
+MiniMovingHead14Ch mover2(37);
+
+#else
+#include "../RgbwSpotlight8ChDemo.h"
+#include "../MiniMovingHead14ChDemo.h"
+
+RgbwSpotlight8ChDemo spotlight1(2);
+RgbwSpotlight8ChDemo spotlight2(4);
+MiniMovingHead14ChDemo mover1(3);
+MiniMovingHead14ChDemo mover2(5);
+#endif
+
+unsigned long previousMillis = 0;
+unsigned long loopStartMillis = 0;
+unsigned long currentMillis = 0;
+
+unsigned short spotCommandLine = 0;
+DmxCommand spotCommands[] = {
+        //{1000, spotlight1.BlueDimming, 255},
+        {1000, spotlight1.Blink, 255},
+        {2000, spotlight1.Blink, 255},
+        {3000, spotlight1.Blink, 255},
+        {4000, spotlight1.Blink, 255}
+};
+
+void setup() {
+#if DEMO == 0
+    DmxSimple.usePin(3);
+  DmxSimple.maxChannel(44);
+#else
+    pinMode(spotlight1.Address, OUTPUT);
+    pinMode(spotlight2.Address, OUTPUT);
+    pinMode(mover1.Address, OUTPUT);
+    pinMode(mover2.Address, OUTPUT);
+#endif
+
+    Serial.begin(9600);
+    Serial.println(3);
+    delay(800);
+    Serial.println(2);
+    delay(800);
+    Serial.println(1);
+    delay(800);
+    Serial.println("GO");
+
+    loopStartMillis = millis();
+}
+
+void loop() {
+    currentMillis = millis() - loopStartMillis;
+    // if (currentMillis - previousMillis >= BPM_IN_MS) {
+    //   previousMillis = currentMillis;
+
+#if DEMO != 0
+    Serial.println(currentMillis);
+#endif
+    // }
+
+    if (spotCommands[spotCommandLine].executionTime <= currentMillis){
+        DmxCommand cmd = spotCommands[spotCommandLine];
+        spotlight1.Set(static_cast<RgbwSpotlight8Ch::Functions>(cmd.function), cmd.value);
+        spotCommandLine++;
+    }
+
+    spotlight1.CleanUp();
+}
