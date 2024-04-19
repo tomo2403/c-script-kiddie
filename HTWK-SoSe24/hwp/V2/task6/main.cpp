1d60d12b65bd3d3f015454c3398bc4d0cd56ce2b
tomo2403 <tomo2403@gmx.de>
2024-04-19 10:22:32 +0200

    implemented task6 V2

---
+++ HTWK-SoSe24/hwp/V2/task6/main.cpp
@@ -0,0 +1,26 @@
+#include <b15f/b15f.h>
+
+int main()
+{
+    // Verbindung zum B15-Board aufbauen
+    B15F &drv = B15F::getInstance();
+
+    std::ofstream file("result.csv");
+
+	if (!file.is_open())
+	{
+		std::cerr << "Konnte die Datei nicht öffnen";
+		return 1;
+	}
+
+    for(uint16_t i= 0; i < 1024; i += 2)
+    {
+        drv.analogWrite0(i);
+        //drv.delay_ms(1);
+        uint16_t o = drv.analogRead(0);
+        std::cout << (i/204.8) << ";" << (o/100.0) << ";" << std::endl;
+        file << (i/204.8) << ";" << (o/100.0) << ";" << std::endl;
+    }
+
+	file.close();
+}
