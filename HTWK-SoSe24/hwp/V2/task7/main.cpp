8503293032c9e1d2ad875af267c9d32ba98a8b7b
tomo2403 <tomo2403@gmx.de>
2024-04-23 13:15:11 +0200

    implemented analog sequence read task7

--- HTWK-SoSe24/hwp/V2/task7/main.cpp
+++ HTWK-SoSe24/hwp/V2/task7/main.cpp
@@ -1,12 +1,11 @@
 #include <b15f/b15f.h>
-#include <string>
 
 int main()
 {
     // Verbindung zum B15-Board aufbauen
     B15F &drv = B15F::getInstance();
 
-    std::ofstream file("result_diode_blue_sperr.csv");
+    std::ofstream file("result_test.csv");
 
 	if (!file.is_open())
 	{
@@ -14,12 +13,13 @@ int main()
 		return 1;
 	}
 
+    std::array<uint16_t, 1024> bufferA, bufferB;
+    drv.analogSequence(0, bufferA.data(), 0, 1, bufferB.data(), 0, 0, 1, 1023);
     for(uint16_t i = 0; i < 1024; i += 2)
     {
-        drv.analogWrite0(i);
-        uint16_t messwert = drv.analogRead(0);
+        uint16_t messwert = bufferB[i];
 
-        float betriebsspannungVolt = i/204.8;
+        float betriebsspannungVolt = bufferA[i]/204.8;
         float messwertVolt = messwert/204.8;
         float stromstaerkeMilliAmpere = messwert/204.8/1000;
         float spannungDiodeVolt = betriebsspannungVolt - messwertVolt;
@@ -28,6 +28,7 @@ int main()
         std::cout << betriebsspannungVolt << ";" << messwertVolt << ";" << stromstaerkeMilliAmpere << ";" << spannungDiodeVolt << std::endl;
         file << betriebsspannungVolt << ";" << messwertVolt << ";" << stromstaerkeMilliAmpere << ";" << spannungDiodeVolt << std::endl;
     }
+    drv.analogWrite0(0);
 
 	file.close();
 }
